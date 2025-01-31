325b3c1e5bdfe9f17eab751f52932cc9f6d417ba
alyson.ramos <alyson.ramos@navita.com.br>
2025-01-31 12:54:03 -0300

    feat: amapi-395 > add

---
+++ i18n.js
@@ -0,0 +1,9 @@
+// // i18n.js
+// import NextI18Next from 'next-i18next';
+
+// // eslint-disable-next-line import/no-anonymous-default-export
+// export default new NextI18Next({
+//   defaultLanguage: 'en',
+//   otherLanguages: ['pt'],
+//   localePath: 'public/locales'
+// });
--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 
 
 const nextConfig: NextConfig = {
-
+    eslint: {
+        dirs: ['pages', 'utils','components'],
+    },
+    // i18n: {
+    //     locales: ['en-US', 'pt-BR'],
+    //     defaultLocale: 'en-US',
+    // },
 };
 
 export default nextConfig;
